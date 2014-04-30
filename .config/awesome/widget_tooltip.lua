2a7012f9b0bfdcbe9d83d686c85d2783e480cbf1
Trygve Aaberge <trygveaa@gmail.com>
2014-04-30 02:25:40 +0200

    awesome: Add support for an icon in widget tooltip

--- .config/awesome/widget_tooltip.lua
+++ .config/awesome/widget_tooltip.lua
@@ -18,16 +18,23 @@ local margin_size = 5
 
 -- Place the tooltip in the top right corner of the screen.
 -- @param self A tooltip object.
-local function set_geometry(self)
+local function set_geometry(self, force)
     local my_geo = self.wibox:geometry()
     local workarea = capi.screen[capi.mouse.screen].workarea
     -- calculate width / height / x / y
     local n_w, n_h = self.textbox:fit(-1, -1)
     n_w = n_w + self.textmargin.left + self.textmargin.right
     n_h = n_h + self.textmargin.top + self.textmargin.bottom
+    if self.iconmargin then
+        n_w = n_w + self.icon_size + self.iconmargin.left + self.iconmargin.right
+        i_h = self.icon_size + self.iconmargin.top + self.iconmargin.bottom
+        if i_h > n_h then
+            n_h = i_h
+        end
+    end
     local n_x = workarea.x + workarea.width - n_w - 6
     local n_y = workarea.y + 4
-    if my_geo.width ~= n_w or my_geo.height ~= n_h or my_geo.x ~= n_x or my_geo.y ~= n_y then
+    if force or my_geo.width ~= n_w or my_geo.height ~= n_h or my_geo.x ~= n_x or my_geo.y ~= n_y then
         self.wibox:geometry({ width = n_w, height = n_h, x = n_x, y = n_y })
     end
 end
@@ -43,7 +50,7 @@ local function show(self)
             data[self].timer:start()
         end
     end
-    set_geometry(self)
+    set_geometry(self, true)
     self.wibox.visible = true
     self.visible = true
 end
@@ -75,6 +82,47 @@ end
 -- @param text New tooltip text, including pango markup.
 local function set_markup(self, text)
     self.textbox:set_markup(text)
+    set_geometry(self)
+end
+
+--- Change displayed icon.
+-- @param self The tooltip object.
+-- @param icon_path Path to new icon.
+-- @param icon_size New icon size.
+local function set_icon(self, icon_path, icon_size)
+    if icon_path == self.icon_path and icon_size == self.icon_size then
+        return
+    end
+
+    if not icon_size then
+        icon_size = self.icon_size or 64
+    end
+
+    if not self.iconbox then
+        self.iconbox = wibox.widget.imagebox(nil, true)
+        self.iconconstraint = wibox.layout.constraint(self.iconbox, "exact", icon_size, icon_size)
+        self.iconmargin = wibox.layout.margin(self.iconconstraint, margin_size, margin_size, margin_size, margin_size)
+        local layout = wibox.layout.fixed.horizontal()
+        layout:add(self.iconmargin)
+        layout:add(self.textmargin)
+        self.wibox:set_widget(layout)
+    elseif icon_size ~= self.icon_size then
+        self.iconconstraint:set_width(icon_size)
+        self.iconconstraint:set_height(icon_size)
+    end
+
+    if icon_path ~= self.icon_path then
+        if self.iconbox:set_image(icon_path) then
+            self.icon_path = icon_path
+        else
+            self.icon_path = nil
+            self.iconbox = nil
+            self.iconmargin = nil
+            self.wibox:set_widget(self.textmargin)
+        end
+    end
+
+    self.icon_size = icon_size
     set_geometry(self)
 end
 
@@ -120,6 +168,7 @@ function widget_tooltip.new(args)
         visible = false,
         set_text = set_text,
         set_markup = set_markup,
+        set_icon = set_icon,
         set_timeout = set_timeout,
         add_to_object = add_to_object,
         remove_from_object = remove_from_object
